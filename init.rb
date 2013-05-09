2d5f0a731b38b426cf3f75e2375d0dc33d892526
octaviogl <root@apache-apps.emergya.info>
2013-05-09 13:05:51 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,24 @@
+# Run initializers
+# Needs to be atop requires because some of them need to be run after initialization
+#Dir["#{File.dirname(__FILE__)}/config/initializers/**/*.rb"].sort.each do |initializer|
+#  require initializer
+#end
+
+# Get messages from locales
+#Dir[File.join("#{File.dirname(__FILE__)}/config/locales/*.yml")].each do |locale|
+#  I18n.load_path.unshift(locale)
+#end
+
+#require 'redmine'
+require 'issues_dates_required_patch'
+#require 'issues_controller_tracker_patch'
+
+Redmine::Plugin.register :redmine_emergya_issue_adjustement do
+  name 'Emergya Issue Adjustement Plugin'
+  author 'ogonzalez'
+  description 'Issue/Tracker behaviour changes adjusting to Emergya workflows'
+  version '0.0.2'
+  author_url 'http://www.emergya.es'
+
+  settings :default => { :trackers => []}, :partial => 'date/settings'
+end
